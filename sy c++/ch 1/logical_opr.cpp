be264568796736a1d3036e557f59c79f61d0d84e
prince0625 <princevagadiya74@gmail.com>
2025-07-21 10:00:19 +0530

    Delete sy c++ directory

--- sy c++/ch 1/logical_opr.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream.h>
-#include<conio.h>
-void main()
-{
-	int a=4 , a=5;
-	cout<<(a>b && a<b); // and operator
-        cout<<(a>b || a<b); // or operator
-	cout<< !(a>b); // not operator
-}
--- sy c++/ch 1/ternary_conditional_opr.cpp
+++
@@ -1,8 +0,0 @@
-#include<iosteam.h>
-#include<conio.h>
-void main()
-{
-	int a=10,b=5;
-	int result = (a>b) ? a:b ;
-        cout<<"the greatest num is"<<result<<endl;
-}
